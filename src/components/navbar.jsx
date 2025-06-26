02c41628929ece297f8fbfdfe62a2c8ce16d72e2
srivyshnavi1505 <srivyshnavip1505@gmail.com>
2025-06-26 16:51:11 +0530

    clean ui

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,38 +1,47 @@
-import React, { useState } from "react";
+import React from "react";
 import { Link, useNavigate } from "react-router-dom";
 import "./navbar.css";
 
-const Navbar = ({loggedin,setloggedin}) => {
+const Navbar = ({ loggedin, setloggedin }) => {
+  const navigate = useNavigate();
 
-  const navigate=useNavigate();
-
-  const handlelogout=()=> {
+  const handlelogout = () => {
     setloggedin(false);
     navigate('/');
-  }
+  };
 
   return (
     <div className="title">
-      <Link to="/"><h1>ChildCare</h1></Link>
-      <div className="navbar">
-        <p><Link to="/">Home</Link></p>
-        <p><Link to="/activities">Activities</Link></p>
-        <p><Link to="/programmes">Programmes</Link></p>
-        {loggedin && <p><Link to="/children">Children</Link></p>}
-      </div>
+  <div className="left-section">
+    <Link to="/"><h1>ChildCare</h1></Link>
+    <div className="navbar">
+      <p><Link to="/">Home</Link></p>
+      <p><Link to="/activities">Activities</Link></p>
+      <p><Link to="/programmes">Programmes</Link></p>
+      {loggedin && <p><Link to="/children">Children</Link></p>}
+    </div>
+  </div>
 
-  <div className="nav-buttons">
+  <div className="nav-content">
     {!loggedin && (
       <>
-        <Link to='/login'><button className="log-button">Login</button></Link>
-        <Link to='/signup'><button className="sign-up-button">Signup</button></Link>
+        <div className="log-button">
+          <Link to='/login'><button>Login</button></Link>
+        </div>
+        <div className="sign-up-button">
+          <Link to='/signup'><button>Signup</button></Link> 
+        </div>
       </>
     )}
-    {loggedin && <button className="logout" onClick={handlelogout}>Logout</button>}
+    {loggedin && (
+      <div className="logout">
+        <button onClick={handlelogout}>Logout</button>
+      </div>
+    )}
   </div>
 </div>
+  );  
 
-  );
 };
 
 export default Navbar;
